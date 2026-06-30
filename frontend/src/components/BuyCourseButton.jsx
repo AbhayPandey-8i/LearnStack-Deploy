e07d23b1062e77ec49db4532e12cc8a770de5e26
Abhay Pandey <pandeyabhay055@gmail.com>
2026-06-30 18:21:05 +0530

    updated features and UI

---
+++ frontend/src/components/BuyCourseButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+const BuyCourseButton = () => {
+  return (
+    
+      <Button className={"bg-black text-white w-full p-4"}>Purchase Course</Button>
+    
+  )
+}
+
+export default BuyCourseButton
---
+++ frontend/src/pages/student/CourseDetail.jsx
@@ -0,0 +1,85 @@
+import BuyCourseButton from '@/components/BuyCourseButton'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
+import { BadgeInfo, Lock, PlayCircle } from 'lucide-react'
+import React from 'react'
+
+const CourseDetail = () => {
+
+    const purchasedCourse = false;
+
+    return (
+        <div className='mt-20 space-y-5' >
+            <div className='bg-[#2D2F31] text-white ' >
+                <div className='max-w-7xl mx-auto py-8 px-4 md:px-8 flex flex-col gap-2 ' >
+                    <h1 className='font-bold text-2xl md:text-3xl' >CourseTitle</h1>
+                    <p className='text-base md:text-lg' >Course subTitle</p>
+                    <p>Created by {" "} <span className='text-[#C0C4FC] underline italic ' >Patel mernstack</span> </p>
+                    <div className='flex items-center gap-2 text-sm'>
+                        <BadgeInfo size={16} />
+                        <p>Last updated 11-06-2026</p>
+                    </div>
+                    <p>Studnets enrolled: 10</p>
+                </div>
+            </div>
+            <div className='max-w-7xl mx-auto my-5 px-4 md:px-8 flex flex-col lg:flex-row justify-between gap-10' >
+                <div className='w-full lg:w-1/2 space-y-5' >
+                    <h1 className='font-bold text-xl md:text-2xl ' >Description</h1>
+                    <p className='text-sm' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, consequuntur! Nobis esse rerum aspernatur quaerat non sapiente eveniet. Voluptatem quae laboriosam facilis neque, officia sapiente possimus perferendis nemo dolor. Officia culpa laboriosam tempore assumenda perferendis vero, facere earum animi sunt.</p>
+                    <Card className={"ring-gray-300"} >
+                        <CardHeader>
+                            <CardTitle>Course Content</CardTitle>
+                            <CardDescription>4 Lectures</CardDescription>
+                        </CardHeader>
+                        <CardContent className='space-y-3' >
+                            {
+                                [1, 2, 3].map((_, idx) => (
+                                    <div key={idx} className='flex items-center gap-3 text-sm' >
+                                        <span>
+                                            {
+                                                true ? (<PlayCircle size={14} />) : <Lock size={14} />
+                                            }
+                                        </span>
+                                        <p>Lecture Title</p>
+                                    </div>
+                                )
+                                )
+                            }
+
+                        </CardContent>
+                    </Card>
+                </div>
+                <div className='w-full lg:w-1/3' >
+                    <Card className={"ring-gray-300"} >
+                        <CardContent className={"flex flex-col p-4"} >
+                            <div className='w-full aspect-video mb-4'>
+                                {/* <ReactPlayer
+                                    width="100%"
+                                    height={"100%"}
+                                    src={course.lectures[0].videoUrl}
+                                    controls={true}
+                                /> */}
+                            </div>
+                            <h1>Lecture Title</h1>
+                            <Separator className="my-2" />
+                            <h1 className='text-lg md:text-xl font-semibold'>Course Price</h1>
+                        </CardContent>
+                        <CardFooter className="flex justify-center p-4">
+                            {
+                                purchasedCourse ? (
+                                    <Button  className="bg-black text-white w-full">Continue Course</Button>
+                                ) : (
+                                    <BuyCourseButton  />
+                                )
+                            }
+                        </CardFooter>
+
+                    </Card>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CourseDetail
