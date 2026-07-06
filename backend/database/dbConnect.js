3eb305b98fb81249ee717eb0cc062cc2f37ac0e4
Abhay Pandey <pandeyabhay055@gmail.com>
2026-07-07 00:58:54 +0530

    updated code

--- backend/database/dbConnect.js
+++ backend/database/dbConnect.js
@@ -4,6 +4,8 @@ const connectDB = async () => {
   try {
      await mongoose.connect(process.env.MONGO_URI);
      console.log("MongoDB Connected Successfully!!");
+     console.log("Connected to DB:", mongoose.connection.name);
+     console.log("Connected to host:", mongoose.connection.host);
   } catch (error) {
     console.log(error)
   }
